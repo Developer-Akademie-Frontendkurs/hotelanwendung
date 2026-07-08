aa199c34225c8f0dfdb0566295ea3f3ed04936cf
Oliver Jung <oljung@posteo.de>
2026-07-08 18:32:22 +0200

    room section
    Co-authored-by: studyguy5 <studyguy5@users.noreply.github.com>

--- src/views/HomeView/Home.ts
+++ src/views/HomeView/Home.ts
@@ -3,6 +3,11 @@ import seezugang from '../../assets/img/icons/seezugang.svg';
 import haubenkueche from '../../assets/img/icons/kitchen.svg';
 import wellness from '../../assets/img/icons/wellness.svg';
 import fam_stroheim from '../../assets/img/familie-stroheim.jpg';
+import double_suite from '../../assets/img/double-suite.jpg';
+import king_bed from '../../assets/img/icons/kingsize.svg';
+import bathroom from '../../assets/img/icons/bathroom.svg';
+import klima_tv from '../../assets/img/icons/klima.svg';
+import gym from '../../assets/img/icons/gym.svg';
 
 export class HomeView extends AbstractView {
     constructor() {
@@ -60,6 +65,46 @@ export class HomeView extends AbstractView {
                     – und schaffen so eine Atmosphäre, in der sich Gäste vom ersten Moment an willkommen
                     und bestens aufgehoben fühlen.
                 </p>
+            </section>
+            <section class="bg-purple-haze-light flex flex-col gap-y-6 py-10 px-4">
+                <div class="flex flex-col gap-y-3 456:gap-y-0 items-center">
+                    <h2 class="font-playfair-display text-24 456:text-32 768:text-48 text-purple-haze-dark font-medium text-center">Zimmer mit Weitblick</h2>
+                    <p class="w-46 456:w-full font-antic-didone text-16 768:text-20 text-purple-haze-dark text-center leading-5">Exklusive Rückzugsorte zwischen Bergen und See</p>
+                </div>
+
+                <div class="bg-white flex flex-col 768:flex-row gap-y-6">
+                    <div>
+                        <img src="${double_suite}" alt="Double Suite">
+                        <p class="px-4 font-antic-didone text-16 text-purple-haze-dark">231€ Nacht/ p. P.</p>
+                    </div>
+
+                    <div class="flex flex-col gap-y-4 px-4">
+                        <h3 class="font-playfair-display text-20 text-purple-haze-dark">Double Suite</h3>
+                        <p class="font-antic-didone text-16 text-purple-haze-dark">Edles Design, viel Raum und der Blick in die Karawanken machen sie zum idealen Rückzugsort für zwei.</p>
+                    </div>
+
+                    <div class="flex flex-col gap-y-4 pb-3">
+                        <div class="flex gap-y-2 px-4">
+                            <img src="${king_bed}" class="w-9.75 h-6" alt="King- size Bett">
+                            <span class="font-antic-didone text-16 text-purple-haze-dark">King- size Bett</span>
+                        </div>
+                        <div class="flex gap-y-2 px-4">
+                            <img src="${bathroom}" class="w-9.75 h-6" alt="Badewanne, Föhn & eigenes WC">
+                            <span class="font-antic-didone text-16 text-purple-haze-dark">Badewanne, Föhn & eigenes WC</span>
+                        </div>
+                        <div class="flex gap-y-2 px-4">
+                            <img src="${klima_tv}" class="w-9.75 h-6" alt="Klimaanlage & TV">
+                            <span class="font-antic-didone text-16 text-purple-haze-dark">Klimaanlage & TV</span>
+                        </div>
+                        <div class="flex gap-y-2 px-4">
+                            <img src="${gym}" class="w-9.75 h-6 mr-2" alt="Gym Zugang">
+                            <span class="font-antic-didone text-16 text-purple-haze-dark">Gym Zugang</span>
+                        </div>
+
+                    </div>
+                </div>
+
+                <button class="bg-purple-haze self-center py-2 px-3 rounded-md text-white">Buchen</button>
             </section>
         `;
     }
